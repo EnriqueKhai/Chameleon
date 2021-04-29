668a568247b051f9c9cef91c1e5f9ea6616a7f5d
Nigel Tan <nigel.tan@u.nus.edu>
2021-04-29 20:32:08 +0800

    Cleaned up help text format issues.

--- deserialize.m
+++ deserialize.m
@@ -1,48 +1,50 @@
 function word_array = deserialize( bit_array , word_len )
-%{
-DESERIALIZE takes in an array of bits and returns
-    an array of words (possibly of size 1), each
-    of length WORD_LEN bits.
+%
+% DESERIALIZE takes in an array of bits and returns
+%     an array of words (possibly of size 1), each
+%     of length WORD_LEN bits.
+% 
+%     Here, a "word" refers to an integer of
+%     WORD_LEN bits. To deserialize an array of
+%     bytes, call deserialize() with WORD_LEN = 8 as
+%     a byte can be represented as an 8-bit integer.
+% 
+%     Every WORD_LEN bits in BIT_ARRAY will be conv-
+%     erted into a word and stored in WORD_ARRAY.
+%     Specifically, each word is a reverse of the
+%     integer described by the corresponding WORD_LEN
+%     bits.
+% 
+%     Assume WORD_LEN = 5. If the first 5 bits in
+%     BIT_ARRAY are [1 0 1 0 0], the the first word
+%     is rev(10100)b = (00101)b = 5.
+% 
+%     WORD_ARRAY, the return value, is simply a con-
+%     catenation of all words reconstructed from
+%     BIT_ARRAY.
+% 
+% 
+% EXAMPLE:
+% 
+%     Assume we are trying to deserialize 5-bit words.
+% 
+%     Let BIT_ARRAY = [0 1 0 0 0 1 0 1 0 0].
+% 
+%     We know that each word is 5-bits long, so there
+%     must be 10 bits / 5 = 2 words inside BIT_ARRAY.
+%     Specifically, the first 5 bits of BIT_ARRAY co-
+%     rrespond to the first word, and the next 5 bits
+%     the second word.
+% 
+%     Then,
+% 
+%     WORD_ARRAY = [rev(01000)b rev(10100)b]
+%                = [   (00010)b    (00101)b]
+%                = [        2           5  ]
+%
+%
+% SEE ALSO: SERIALIZE.
 
-    Here, a "word" refers to an integer of
-    WORD_LEN bits. To deserialize an array of
-    bytes, call deserialize() with WORD_LEN = 8 as
-    a byte can be represented as an 8-bit integer.
-
-    Every WORD_LEN bits in BIT_ARRAY will be conv-
-    erted into a word and stored in WORD_ARRAY.
-    Specifically, each word is a reverse of the
-    integer described by the corresponding WORD_LEN
-    bits.
-
-    Assume WORD_LEN = 5. If the first 5 bits in
-    BIT_ARRAY are [1 0 1 0 0], the the first word
-    is rev(10100)b = (00101)b = 5.
-
-    WORD_ARRAY, the return value, is simply a con-
-    catenation of all words reconstructed from
-    BIT_ARRAY.
-
-
-EXAMPLE:
-
-    Assume we are trying to deserialize 5-bit words.
-
-    Let BIT_ARRAY = [0 1 0 0 0 1 0 1 0 0].
-
-    We know that each word is 5-bits long, so there
-    must be 10 bits / 5 = 2 words inside BIT_ARRAY.
-    Specifically, the first 5 bits of BIT_ARRAY co-
-    rrespond to the first word, and the next 5 bits
-    the second word.
-
-    Then,
-
-    WORD_ARRAY = [rev(01000)b rev(10100)b]
-               = [   (00010)b    (00101)b]
-               = [        2           5  ]
-
-%}
     num_words  = size(bit_array, 2) / word_len;
     word_array = zeros(1, num_words);
     
--- serialize.m
+++ serialize.m
@@ -1,48 +1,50 @@
 function bit_array = serialize( word_array , word_len )
-%{
-SERIALIZE takes in an array of words (possibly of
-    size 1), each of length WORD_LEN bits, and
-    returns an array of bits.
+%
+% SERIALIZE takes in an array of words (possibly of
+%     size 1), each of length WORD_LEN bits, and
+%     returns an array of bits.
+% 
+%     Here, a "word" refers to an integer of
+%     WORD_LEN bits. To serialize an array of bytes,
+%     call serialize() with WORD_LEN = 8 as a byte
+%     can be represented as an 8-bit integer.
+% 
+%     Each word in WORD_ARRAY is converted into a
+%     subarray of WORD_LEN bits. Specifically , the
+%     subarray corresponding to each word is the re-
+%     verse of said word's binary representation.
+% 
+%     Consider the decimal number 11 = (1011)b. To
+%     convert it into a subarray of 8 bits, we first
+%     left pad (1011)b with zeros to make it 8 bits
+%     long, i.e. (00001011)b. Then, we simply rever-
+%     se it, i.e. (11010000)b.
+% 
+%     BIT_ARRAY, the return value, is simply a conc-
+%     atenation of all subarrays.
+% 
+% 
+% EXAMPLE:
+% 
+%     Assume we are trying to serialize 5-bit words.
+% 
+%     Calling serialize([2, 5], 5) returns the array
+%     [0 1 0 0 0 1 0 1 0 0].
+% 
+%     2 = ( 10)b = (00010)b.
+%     5 = (101)b = (00101)b.
+% 
+%     Then,   [2, 5] === [(00010)b, (00101)b].
+% 
+%     Reversing each individual subarray in-place,
+%     we have [2, 5] === [(01000)b, (10100)b].
+% 
+%     Now, we concatenate (01000)b and (10100)b to
+%     get the return value [0 1 0 0 0 1 0 1 0 0].
+%
+%
+% SEE ALSO: DESERIALIZE.
 
-    Here, a "word" refers to an integer of
-    WORD_LEN bits. To serialize an array of bytes,
-    call serialize() with WORD_LEN = 8 as a byte
-    can be represented as an 8-bit integer.
-
-    Each word in WORD_ARRAY is converted into a
-    subarray of WORD_LEN bits. Specifically , the
-    subarray corresponding to each word is the re-
-    verse of said word's binary representation.
-
-    Consider the decimal number 11 = (1011)b. To
-    convert it into a subarray of 8 bits, we first
-    left pad (1011)b with zeros to make it 8 bits
-    long, i.e. (00001011)b. Then, we simply rever-
-    se it, i.e. (11010000)b.
-
-    BIT_ARRAY, the return value, is simply a conc-
-    atenation of all subarrays.
-
-
-EXAMPLE:
-
-    Assume we are trying to serialize 5-bit words.
-
-    Calling serialize([2, 5], 5) returns the array
-    [0 1 0 0 0 1 0 1 0 0].
-
-    2 = ( 10)b = (00010)b.
-    5 = (101)b = (00101)b.
-
-    Then,   [2, 5] === [(00010)b, (00101)b].
-
-    Reversing each individual subarray in-place,
-    we have [2, 5] === [(01000)b, (10100)b].
-
-    Now, we concatenate (01000)b and (10100)b to
-    get the return value [0 1 0 0 0 1 0 1 0 0].
-
-%}
     num_words = size(word_array, 2);
     bit_array = zeros(1, num_words * word_len);
     
